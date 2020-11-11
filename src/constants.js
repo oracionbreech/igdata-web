f29cb6fc3a5c1a6e6606b472895c87b5b641d330
breechoracion <oracionbre@gmail.com>
2020-11-12 00:34:09 +0800

    replaced heroku server with aws ec2 instance

--- src/constants.js
+++ src/constants.js
@@ -4,8 +4,8 @@ const PROD = "PROD";
 
 const CURRENTENV = PROD;
 
-const herokuProdUrl =
+const prodUrl =
   "http://ec2-13-250-102-178.ap-southeast-1.compute.amazonaws.com/";
 const localDevUrl = "http://localhost:5000";
 
-export const apiUri = CURRENTENV === DEV ? localDevUrl : herokuProdUrl;
+export const apiUri = CURRENTENV === DEV ? localDevUrl : prodUrl;
