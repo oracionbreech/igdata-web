681c933dc303efcc617fa808775ced007b97ce04
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2021-02-19 01:04:04 +0800

    fix: prod url

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ const PROD = 'PROD';
 
 const CURRENTENV = PROD;
 
-const prodUrl = 'http://api.igdata.xyz';
-const localDevUrl = 'http://localhost:5000';
+const prodUrl = 'http://192.64.115.144:5000';
+const localDevUrl = 'http://localhost:3000';
 
 export const apiUri = CURRENTENV === DEV ? localDevUrl : prodUrl;
