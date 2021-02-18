d41f7bd5e728a3c739cd77a498765792875ebe7e
Breech Alcres Paul Oracion <oracionbre@gmail.com>
2021-02-19 00:00:45 +0800

    fix: renamed prod api url

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
 // ENV's
-const DEV = "DEV";
-const PROD = "PROD";
+const DEV = 'DEV';
+const PROD = 'PROD';
 
 const CURRENTENV = PROD;
 
-const prodUrl = "http://18.136.123.125:8080";
-const localDevUrl = "http://localhost:5000";
+const prodUrl = 'http://localhost:8080';
+const localDevUrl = 'http://localhost:5000';
 
 export const apiUri = CURRENTENV === DEV ? localDevUrl : prodUrl;
